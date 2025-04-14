71ad72807719791365cf2b14200274fc252957e1
Arda Boz <ardaboz4317@gmail.com>
2025-04-14 19:18:25 -0400

    Added Student class
    Created Student class and implemented basic fields, constructor and getters and setters.

---
+++ projects/student-course-registration-system/src/Student.java
@@ -0,0 +1,23 @@
+public class Student {
+    // Encapsulated fields
+    private String studentID;
+    private String studentName;
+    // Constructor
+    Student(String studentID, String studentName) {
+        this.studentID = studentID;
+        this.studentName = studentName;
+    }
+    // Getters and setters
+    public String getStudentID() {
+        return studentID;
+    }
+    public String getStudentName() {
+        return studentName;
+    }
+    public void setStudentID(String studentID) {
+        this.studentID = studentID;
+    }
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+}
