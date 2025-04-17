b1209cd972e9513f775e410428d52d74f026c57e
Arda Boz <ardaboz4317@gmail.com>
2025-04-16 23:13:19 -0400

    Added addTask method
    This method takes input from the user, checks if such task exists in HashSet. If not, creates new task. If category doesn't exist, creates a category with ArrayList of Task objects. Then adds task to ArrayList mapped to the category. Then it adds it to PriorityQueue for sorting. Then adds a notification.
    
    I also changed the way PriorityQueue works by adding a custom comparator.

--- projects/task-management-system/src/DataManager.java
+++ projects/task-management-system/src/DataManager.java
@@ -1,3 +1,4 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
 import java.util.*;
 
 public class DataManager {
@@ -11,12 +12,51 @@ public class DataManager {
     // Value: List of tasks within that category
     // Usage: To easily retrieve tasks by category and manage tasks grouped together.
 
-    private PriorityQueue<Task> taskQueue = new PriorityQueue<>();
+    PriorityQueue<Task> taskQueue = new PriorityQueue<>(new Comparator<Task>() {
+        @Override
+        public int compare(Task t1, Task t2) {
+            return Integer.compare(t1.getTaskPriority(), t2.getTaskPriority()); // Sorting by age
+        }
+    });
     // Purpose: Maintain tasks sorted by priority (or urgency)
     // so that the most urgent task is always available.
-    // Requirement: Create a custom comparator to sort tasks (In descending order of priority).
+    // custom comparator to sort tasks (In descending order of priority).
 
     private LinkedList<String> notifications = new LinkedList<>();
     // Purpose: Log notifications (e.g., "Task X added", "Task Y marked complete").
     // Usage: Maintain a history of actions in the order they happened.
+
+    // Methods
+
+    // Add a task
+    public void addTask(Scanner scanner) {
+        // Take input
+        System.out.println("\nAdding a task");
+        System.out.print("ID: ");
+        String inputID = scanner.nextLine();
+        System.out.print("Description: ");
+        String inputDescription = scanner.nextLine();
+        System.out.print("Category: ");
+        String inputCategory = scanner.nextLine();
+        System.out.print("Priority: ");
+        int inputPriority = scanner.nextInt();
+
+        if (taskIDs.contains(inputID)) {
+            System.out.println("Task with that ID already exists!");
+        } else {
+            // Create new task
+            Task newTask = new Task(inputID, inputDescription, inputCategory, inputPriority, false);
+
+            // If category does'nt exits, create a category with ArrayList
+            tasksByCategory.putIfAbsent(inputCategory, new ArrayList<>());
+            // Add task to ArrayList mapped to that category
+            tasksByCategory.get(inputCategory).add(newTask);
+
+            // Add it to Queue for sorting
+            taskQueue.add(newTask);
+
+            // Add a notification
+            notifications.add("Task " + inputID + " created!");
+        }
+    }
 }
